c53ae09fdd39fe6a8403c473ccda006386a0cf09
Ari <52987058+rarebeatle@users.noreply.github.com>
2020-10-24 15:51:21 -0600

    commit1

---
+++ PrjInyeccionSimple/src/writer/NiceWriter.java
@@ -0,0 +1,12 @@
+package writer;
+import org.springframework.stereotype.Service;
+@Service
+public class NiceWriter implements IWriter{
+
+	@Override
+	public void writer(String s) {
+		System.out.println("The String is"+s);
+		
+	}
+
+}
---
+++ PrjInyeccionSimple/src/writer/Writer.java
@@ -0,0 +1,10 @@
+package writer;
+
+public class Writer implements IWriter{
+
+	@Override
+	public void writer(String s) {
+		System.out.println("+++++-->" +s );		
+	}
+
+}
